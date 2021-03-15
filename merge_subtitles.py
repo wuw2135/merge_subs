6096989ae24241c524e242acd8321a426291161d
wuw2135 <80589831+wuw2135@users.noreply.github.com>
2021-03-16 07:23:12 +0900

    Update merge_subtitles.py

--- merge_subtitles.py
+++ merge_subtitles.py
@@ -6,44 +6,53 @@ import shutil
 from pytube import YouTube
 import re
 
-def edit_filecontent(count):
-    for i in range (count-1):
-        fname = str(i+1)+".srt"
-        filein = open(fname,"r",encoding="utf-8")
-        a = filein.readlines()
-        fileout = open(fname,"w",encoding="utf-8")
-        b = "".join(a[3:])
-        fileout.write(b)
+def edit_filecontent():
+    Path = os.getcwd() 
+    all_file_list = os.listdir(Path)
 
-        filein.close()
-        fileout.close()
+    for file_name in all_file_list:
+        fname = os.path.splitext(file_name)[0]
+        ftype = os.path.splitext(file_name)[1]
+        new_filename = fname+r".srt"
+        if ftype == r".vtt":
+            os.rename(os.path.join(Path, file_name), os.path.join(Path, new_filename))
+            filein = open(new_filename,"r",encoding="utf-8")
+            a = filein.readlines()
+            fileout = open(new_filename,"w",encoding="utf-8")
+            b = "".join(a[4:])
+            fileout.write(b)
 
-        f = open(fname,encoding="utf-8")
-        index = 1
-        line = f.readline()
-        with open("file"+str(i+1)+".srt",'a',encoding="utf-8") as new_file:
-            while line:
-                if "-->" in line:
-                    new_file.write(f"{str(index)}\n{line}")
-                    index += 1
-                else:
-                    new_file.write(line)
-                line = f.readline()
-        f.close()
-        os.remove(fname)
+            filein.close()
+            fileout.close()
 
+            f = open(new_filename,encoding="utf-8")
+            index = 1
+            line = f.readline()
+            with open("_"+new_filename,"a",encoding="utf-8") as new_file:
+                while line:
+                    if "-->" in line:
+                        new_file.write(f"{str(index)}\n{line}")
+                        index += 1
+                    else:
+                        new_file.write(line)
+                    line = f.readline()
+                f.close()
+                os.remove(new_filename)
+                            
+    return Path,all_file_list
 
-def merge_subs(count):
-    allsubs = list()
-    for i in range (count-1):
-        fname = "file"+str(i+1)+".srt"
-        fileread = open(fname,"r",encoding="utf-8")
-        sub = list(srt.parse(fileread, ignore_errors=False))
-        for j in range (len(sub)):
-            allsubs.append(sub[j])
-        fileread.close()
+def merge_subs(count,all_file_list):
+    mergesub = list()
+    for file_name in all_file_list:
+        ftype = os.path.splitext(file_name)[1]
+        if ftype == r".srt":
+            fileread = open(file_name,"r",encoding="utf-8")
+            mergesub_list = list(srt.parse(fileread,ignore_errors=False))
+            for i in range (len(mergesub_list)):
+                mergesub.append(mergesub_list[i])
+            fileread.close()
 
-    merge = list(srt.sort_and_reindex(allsubs))
+    merge = list(srt.sort_and_reindex(mergesub))
     
     total_file = open("merge.srt","w",encoding="utf-8")
     total_file.writelines(srt.compose(merge))
@@ -51,7 +60,7 @@ def merge_subs(count):
 
 def add_folder(url):
     yt = YouTube(url)
-    path2 = "./" + re.sub(r'[\/:*"<>|]',"",yt.title)
+    path2 = "./" + re.sub(r'[\/:*"<>|]',"_",yt.title)
     if os.path.isdir(path2) == False: 
         os.mkdir(path2)
     return path2
@@ -59,10 +68,10 @@ def add_folder(url):
 def files_move(Path,path2):
     all_file_list = os.listdir(Path)
 
-    for fname in all_file_list:
-        ftype = os.path.splitext(fname)[1]
+    for file_name in all_file_list:
+        ftype = os.path.splitext(file_name)[1]
         if ftype == r".srt" or ftype == r".mp4" or ftype == r".mkv" or ftype == r".ogg" or ftype == r".webm" or ftype == r".flv" or ftype == r".txt":
-            shutil.move(fname,path2)
+            shutil.move(file_name,path2)
 
 def convert_txt():
     f = open("merge.srt","r",encoding="utf-8")
@@ -84,23 +93,23 @@ def main():
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-    Path = os.getcwd() 
-    all_file_list = os.listdir(Path)
+    edit_filecontent()
+    Path = edit_filecontent()[0]
+    all_file_list = edit_filecontent()[1]
+
     count = 1
-
     for file_name in all_file_list:
-        new_fname = str(count)
         ftype = os.path.splitext(file_name)[1]
-        if ftype == r".vtt":
-            os.rename(os.path.join(Path, file_name), os.path.join(Path, new_fname+r".srt"))
-            count = count + 1
+        if ftype == r".srt":
+            count = count +1
 
-    edit_filecontent(count)
     if count >= 3:
-        merge_subs(count)
+        merge_subs(count,all_file_list)
         convert_txt()
+    
     add_folder(url)
     path2 = add_folder(url)
+
     files_move(Path,path2)
 
 if __name__ == "__main__": 
